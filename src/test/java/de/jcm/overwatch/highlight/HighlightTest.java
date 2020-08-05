e0371898f6ad3f621d98097323797002bd34b31c
JCM <jacama2009@gmail.com>
2020-08-05 16:38:07 +0200

    Fix indentation in HighlightTest.java

--- src/test/java/de/jcm/overwatch/highlight/HighlightTest.java
+++ src/test/java/de/jcm/overwatch/highlight/HighlightTest.java
@@ -18,56 +18,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HighlightTest
 {
-    @BeforeAll
-    static void before() throws IOException, BackingStoreException
-    {
-        Assumptions.assumeTrue(findHighlights().count()>0, "No highlights found!");
-    }
+	@BeforeAll
+	static void before() throws IOException, BackingStoreException
+	{
+		Assumptions.assumeTrue(findHighlights().count()>0, "no highlights found!");
+	}
 
-    @ParameterizedTest
-    @MethodSource(value = "findHighlights")
-    void test(File highlightFile) throws IOException
-    {
-        OverwatchHighlight highlight = new OverwatchHighlight(highlightFile);
+	@ParameterizedTest
+	@MethodSource(value = "findHighlights")
+	void test(File highlightFile) throws IOException
+	{
+		OverwatchHighlight highlight = new OverwatchHighlight(highlightFile);
 
-        System.out.println("highlight.getType() = " + highlight.getType());
-        System.out.println("highlight.getTitle() = " + highlight.getTitle());
-        System.out.println("highlight.getHero() = " + highlight.getHero());
-        System.out.println("highlight.getMap() = " + highlight.getMap());
-        System.out.println("highlight.getSkin() = " + highlight.getSkin());
+		System.out.println("highlight.getType() = " + highlight.getType());
+		System.out.println("highlight.getTitle() = " + highlight.getTitle());
+		System.out.println("highlight.getHero() = " + highlight.getHero());
+		System.out.println("highlight.getMap() = " + highlight.getMap());
+		System.out.println("highlight.getSkin() = " + highlight.getSkin());
 
-        assertNotNull(highlight.getType(), "highlight has unknown type");
-        assertNotNull(highlight.getHero(), "highlight has unknown hero");
-        assertNotNull(highlight.getMap(), "highlight has unknown map");
-        assertNotNull(highlight.getSkin(), "highlight has unknown skin");
+		assertNotNull(highlight.getType(), "highlight has unknown type");
+		assertNotNull(highlight.getHero(), "highlight has unknown hero");
+		assertNotNull(highlight.getMap(), "highlight has unknown map");
+		assertNotNull(highlight.getSkin(), "highlight has unknown skin");
 
-        if(highlight.getHero() != null && highlight.getSkin() != null)
-        {
-            assertEquals(highlight.getHero(), highlight.getSkin().getHero(), "hero and hero of skin do not match");
-        }
-    }
+		if(highlight.getHero() != null && highlight.getSkin() != null)
+		{
+			assertEquals(highlight.getHero(), highlight.getSkin().getHero(), "hero and hero of skin do not match");
+		}
+	}
 
-    static Stream<File> findHighlights() throws IOException, BackingStoreException
-    {
-        File directory = new File(System.getProperty("user.home"), "Documents\\Overwatch\\videos\\overwatch\\");
+	static Stream<File> findHighlights() throws IOException, BackingStoreException
+	{
+		File directory = new File(System.getProperty("user.home"), "Documents\\Overwatch\\videos\\overwatch\\");
 
-        File overwatchConfig =
-                new File(System.getProperty("user.home"), "Documents\\Overwatch\\Settings\\Settings_v0.ini");
-        if(overwatchConfig.exists())
-        {
-            Config.getGlobal().setEscape(false);
-            IniPreferences iniPreferences = new IniPreferences(new FileInputStream(overwatchConfig));
-            if(iniPreferences.nodeExists("MovieExport.1"))
-            {
-                directory = new File(iniPreferences.node("MovieExport.1").get("VideoPath",
-                        System.getProperty("user.home")+"\\Documents\\Overwatch\\videos\\overwatch\\")
-                                                   .replace("\"", ""));
-            }
-        }
+		File overwatchConfig =
+				new File(System.getProperty("user.home"), "Documents\\Overwatch\\Settings\\Settings_v0.ini");
+		if(overwatchConfig.exists())
+		{
+			Config.getGlobal().setEscape(false);
+			IniPreferences iniPreferences = new IniPreferences(new FileInputStream(overwatchConfig));
+			if(iniPreferences.nodeExists("MovieExport.1"))
+			{
+				directory = new File(iniPreferences.node("MovieExport.1")
+						                     .get("VideoPath",
+						                          System.getProperty("user.home") + "\\Documents\\Overwatch\\videos\\overwatch\\")
+						                     .replace("\"", ""));
+			}
+		}
 
-        if(!directory.exists())
-            return Stream.empty();
+		if(!directory.exists())
+			return Stream.empty();
 
-        return Stream.of(Objects.requireNonNull(directory.listFiles((f, s) -> s.endsWith(".mp4"))));
-    }
+		return Stream.of(Objects.requireNonNull(directory.listFiles((f, s) -> s.endsWith(".mp4"))));
+	}
 }
